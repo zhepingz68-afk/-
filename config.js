80d631a459c9d7b0dfdb2fbf853b17e7e556b553
TP <zhepingz68@gmail.com>
2026-09-11 17:11:57 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Supabase の管理画面から取得した値をここに入れてください。
 // service_role key は絶対に入れないでください。ブラウザでは anon key を使います。
 window.ENERGY_BATTLE_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://uknebjoornizscxbccnj.supabase.co",
+  anonKey: "sb_publishable_ArwpiiaCm_RunkguAkBC0g_7s9_oU4A"
 };
